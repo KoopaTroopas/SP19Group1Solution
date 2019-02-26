27ff78613415d40717d59ca24147cf929d626d88
Tyler Kenney <tgkenney@mix.wvu.edu>
2019-02-26 11:55:10 -0500

    Initial Commit

---
+++ SP19Group1Solution/SP19MVCWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SP19MVCWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
